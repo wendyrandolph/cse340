46c5f76e3caf6a07c7b32e8c1fcf24e18a4cf1c5
Wendy Randolph <43681021+wendyrandolph@users.noreply.github.com>
2020-09-25 19:56:44 -0600

    Add files via upload

---
+++ template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MOTORS</title>
+    <link rel="stylesheet" href="css/main.css" media="screen">
+
+</head>
+
+<body>
+
+    <header id="page_header">
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/header.php'; ?>
+    </header>
+
+    <nav id="page_nav">
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/nav.php'; ?>
+    </nav>
+
+    <h1 id="title"> Welcome to PHP Motors! </h1>
+
+
+    <main>
+    <h3> DMC Delorean </h3> 
+    <p> 3 Cup holders </p> 
+    <p> Superman doors </p> 
+    <p> Fuzzy dice! </p> 
+
+    </main>
+    <footer id="page_footer">
+        <?php require $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/snippets/footer.php'; ?>
+    </footer>
+</body>
+
+</html>
